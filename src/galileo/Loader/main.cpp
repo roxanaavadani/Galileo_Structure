830fb1f00dfb963821fe9b492892a5115ef0a4b0
horatiu.condrea <c.horias3@gmail.com>
2021-08-10 14:38:21 +0300

    Loader - implementation guide

--- src/galileo/Loader/main.cpp
+++ src/galileo/Loader/main.cpp
@@ -1,5 +1,10 @@
 
 int main(int argc, char **argv)
 {
-
+	// 1. Create and instatiate a QApplication variable
+  // 2. Get the Application instance
+  // 3. Instatiate the ApplicationModel
+  // 4. Provide the ApplicationModel to the Application
+  // 5. Start the Application
+  // 6. Execute the QApplication
 }
